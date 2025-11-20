081b17a9473bd6180401a1e9a05e53ab8a14466f
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2025-11-20 11:21:35 -0300

    fix subgraph config

--- packages/subgraph/src/bueno-token.ts
+++ packages/subgraph/src/bueno-token.ts
@@ -1,15 +1,11 @@
 import {
   Approval as ApprovalEvent,
-  RoleAdminChanged as RoleAdminChangedEvent,
-  RoleGranted as RoleGrantedEvent,
-  RoleRevoked as RoleRevokedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
   Transfer as TransferEvent
 } from "../generated/BuenoToken/BuenoToken"
 import {
   Approval,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
+  OwnershipTransferred,
   Transfer
 } from "../generated/schema"
 
@@ -28,49 +24,14 @@ export function handleApproval(event: ApprovalEvent): void {
   entity.save()
 }
 
-export function handleRoleAdminChanged(
-  event: RoleAdminChangedEvent
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
 ): void {
-  let entity = new RoleAdminChanged(
+  let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleGranted(
-  event: RoleGrantedEvent
-): void {
-  let entity = new RoleGranted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleRevoked(
-  event: RoleRevokedEvent
-): void {
-  let entity = new RoleRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
